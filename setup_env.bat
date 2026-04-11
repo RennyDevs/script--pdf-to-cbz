f535dd5290c4c025c3cdf5583f249dd7a74d2afb
RennyDev <rennyarmando@gmail.com>
2026-04-11 11:40:05 -0400

    Feat: Add script to setup in windows

---
+++ setup_env.bat
@@ -0,0 +1,19 @@
+@echo off
+set VENV=.venv
+
+REM Crear venv si no existe
+if not exist "%VENV%" (
+    python3 -m venv "%VENV%"
+)
+
+REM Instalar paquetes
+"%VENV%\Scripts\pip.exe" install --upgrade pip
+"%VENV%\Scripts\pip.exe" install -r requirements.txt
+
+echo.
+echo Entorno creado e paquetes instalados en %VENV%.
+echo Para activarlo en CMD:
+echo    %VENV%\Scripts\activate.bat
+echo Para PowerShell:
+echo    %VENV%\Scripts\Activate.ps1
+pause
